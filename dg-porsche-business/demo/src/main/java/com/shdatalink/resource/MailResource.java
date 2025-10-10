aac1657959e0e1f009bf9f4168dc552e739e9a34
huyulong <huyulong@shdatalink.com>
2025-10-10 15:58:15 +0800

    添加邮件相关依赖

---
+++ dg-porsche-business/demo/src/main/java/com/shdatalink/resource/MailResource.java
@@ -0,0 +1,45 @@
+package com.shdatalink.resource;
+
+import com.shdatalink.utils.MailUtil;
+import io.quarkus.mailer.MailTemplate;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.smallrye.common.annotation.Blocking;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 邮件
+ */
+@Path("/mail")
+public class MailResource {
+
+    @Inject
+    MailUtil mailUtil;
+
+    @Inject
+    @Location("email/hello")
+    Template hello;
+
+    @Path("/send")
+    @GET
+    public boolean send() {
+        mailUtil.sendText(List.of("huyulong@shdatalink.com", "809204304@qq.com"), "测试邮件主题", "测试邮件内容");
+        return true;
+    }
+
+    @Path("/send/template")
+    @GET
+    public boolean sendTemplate() {
+        Map<String, Object> data = Map.of("name", "huyulong");
+        mailUtil.sendTemplate(List.of("huyulong@shdatalink.com", "809204304@qq.com"), "测试邮件主题", hello, data);
+        return true;
+    }
+
+
+}
---
+++ dg-porsche-framework/porsche-framework-extension/porsche-framework-extension-mail/src/main/java/com/shdatalink/utils/MailUtil.java
@@ -0,0 +1,80 @@
+package com.shdatalink.utils;
+
+import com.shdatalink.framework.common.utils.ArgUtil;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Quarkus邮件工具类
+ */
+@Slf4j
+@ApplicationScoped
+public class MailUtil {
+
+    @Inject
+    Mailer mailer;
+
+
+    /**
+     * 发送简单文本邮件
+     *
+     * @param to      收件人邮箱
+     * @param subject 邮件主题
+     * @param content 邮件内容
+     */
+    public void sendText(List<String> to, String subject, String content) {
+        ArgUtil.judgeEmpty(to, "收件人邮箱不能为空");
+        ArgUtil.judgeBlank(subject, "邮件主题不能为空");
+
+        Mail mail = Mail.withText(to.getFirst(), subject, content);
+        if (to.size() > 1) {
+            to.stream().skip(1).forEach(mail::addTo);
+        }
+        mailer.send(mail);
+
+    }
+
+    /**
+     * 发送HTML邮件
+     *
+     * @param to      收件人邮箱
+     * @param subject 邮件主题
+     * @param html    HTML内容
+     */
+    public void sendHtml(List<String> to, String subject, String html) {
+        ArgUtil.judgeEmpty(to, "收件人邮箱不能为空");
+        ArgUtil.judgeBlank(subject, "邮件主题不能为空");
+        ArgUtil.judgeBlank(html, "HTML内容不能为空");
+
+        Mail mail = Mail.withHtml(to.getFirst(), subject, html);
+        if (to.size() > 1) {
+            to.stream().skip(1).forEach(mail::addTo);
+        }
+        mailer.send(mail);
+    }
+
+    /**
+     * 发送模板邮件
+     *
+     * @param to       收件人邮箱
+     * @param subject  邮件主题
+     * @param template Qute模板
+     * @param data     模板数据
+     */
+    public void sendTemplate(List<String> to, String subject, Template template, Map<String, Object> data) {
+        ArgUtil.judgeNull(template, "模板不能为空");
+
+        TemplateInstance instance = template.data(data);
+        String htmlContent = instance.render();
+        sendHtml(to, subject, htmlContent);
+    }
+
+}
