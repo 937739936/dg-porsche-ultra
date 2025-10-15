4078b4390e51aee920ac5a1516d1d2e93de4ea3e
huyulong <huyulong@shdatalink.com>
2025-10-15 14:51:16 +0800

    定时任务

--- dg-porsche-business/porsche-sip-server/src/main/java/com/shdatalink/sip/server/module/device/schedule/DeviceSchedule.java
+++ dg-porsche-business/porsche-sip-server/src/main/java/com/shdatalink/sip/server/module/device/schedule/DeviceSchedule.java
@@ -1,5 +1,10 @@
 package com.shdatalink.sip.server.module.device.schedule;
 
+import com.shdatalink.sip.server.gb28181.core.bean.constants.InviteTypeEnum;
+import com.shdatalink.sip.server.module.device.entity.Device;
+import com.shdatalink.sip.server.module.device.entity.DeviceChannel;
+import com.shdatalink.sip.server.module.device.enums.ProtocolTypeEnum;
+import com.shdatalink.sip.server.module.device.event.DeviceOnlineEvent;
 import com.shdatalink.sip.server.module.device.service.SseClient;
 import io.quarkus.scheduler.Scheduled;
 import io.smallrye.common.annotation.RunOnVirtualThread;
@@ -7,28 +12,45 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Slf4j
 @ApplicationScoped
 public class DeviceSchedule {
 
-    @Inject
-    SseClient sseClient;
-
-//    @Scheduled(cron = "0 */10 * * * *")
-//    @Scheduled(cron = "*/10 * * * * ?")
-//    @RunOnVirtualThread
-    void test() {
-        log.info("test");
-        sseClient.sendDeviceMessage(LocalDateTime.now());
+    /**
+     * 设备在线状态查询任务
+     */
+    @Scheduled(cron = "0 0/10 * * * ?")
+    @RunOnVirtualThread
+    public void onlineCheck() {
+        log.info("onlineCheck start");
     }
 
-//    @Scheduled(cron = "*/10 * * * * ?")
-//    @RunOnVirtualThread
-//    void test2() {
-//        log.info("test2");
-//        sseClient.sendDeviceMessage(LocalDateTime.now());
-//    }
+    @Scheduled(cron = "0 0/15 * * * ?")
+    @RunOnVirtualThread
+    public void snapshot() {
+        log.info("snapshot start");
+    }
+
+    @Scheduled(cron = "0 0/2 * * * ?")
+    @RunOnVirtualThread
+    public void snapshotExistsStream() {
+        log.info("snapshotExistsStream start");
+    }
+
+    /**
+     * 刷新订阅
+     */
+    @Scheduled(cron = "0 0/10 * * * ?")
+    @RunOnVirtualThread
+    public void updateSubscribeExpires() {
+        log.info("updateSubscribeExpires start");
+    }
 }
