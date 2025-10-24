8eb9e1c07d57f1fdfdb7d9b665348e034929ed51
zhanghang <zhanghang@shdatalink.com>
2025-10-24 11:18:21 +0800

    缩短播放链接

--- dg-porsche-business/porsche-sip-server/src/main/java/com/shdatalink/sip/server/media/MediaSignService.java
+++ dg-porsche-business/porsche-sip-server/src/main/java/com/shdatalink/sip/server/media/MediaSignService.java
@@ -1,16 +1,9 @@
 package com.shdatalink.sip.server.media;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
 import com.shdatalink.sip.server.config.SipConfigProperties;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.commons.codec.digest.DigestUtils;
-
-import java.util.Date;
 
 import static com.shdatalink.sip.server.common.constants.CommonConstants.PLAY_DEFAULT_EXPIRE;
 
@@ -25,7 +18,7 @@ public class MediaSignService {
 
     public String sign(String stream, int expire) {
         long expireAt = System.currentTimeMillis() / 1000 + expire;
-        return DigestUtils.md2Hex(String.format("%s:%d:%s", stream, expireAt, sipConfigProperties.media().secret())) + "&expire=" + expireAt;
+        return DigestUtils.md5Hex(String.format("%s:%d:%s", stream, expireAt, sipConfigProperties.media().secret())).substring(8, 24) + "&expire=" + expireAt;
     }
 
     public boolean verify(String streamId, String token, Integer expire) {
