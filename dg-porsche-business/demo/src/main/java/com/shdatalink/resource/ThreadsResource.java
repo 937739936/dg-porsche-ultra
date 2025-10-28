eebef5e45b3fb31e0caa3fbb2dc1c73d8904f845
huyulong <huyulong@shdatalink.com>
2025-10-28 16:47:23 +0800

    自定义线程池示例

--- dg-porsche-business/demo/src/main/java/com/shdatalink/resource/ThreadsResource.java
+++ dg-porsche-business/demo/src/main/java/com/shdatalink/resource/ThreadsResource.java
@@ -1,10 +1,15 @@
 package com.shdatalink.resource;
 
 import io.quarkus.virtual.threads.VirtualThreads;
+import io.smallrye.context.api.ManagedExecutorConfig;
+import io.smallrye.context.api.NamedInstance;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.context.ThreadContext;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
@@ -18,11 +23,18 @@ public class ThreadsResource {
     @VirtualThreads
     ExecutorService vThreads;
 
+    @Inject
+    @Named("app-executor") // 注入自定义线程池
+    ExecutorService appExecutor;
+
 
     @Path("/execute")
     @GET
     public LocalDateTime execute() {
         vThreads.execute(() -> {
+            log.info("Hello from a virtual thread!");
+        });
+        appExecutor.execute(() -> {
             log.info("Hello from a virtual thread!");
         });
         return LocalDateTime.now();
